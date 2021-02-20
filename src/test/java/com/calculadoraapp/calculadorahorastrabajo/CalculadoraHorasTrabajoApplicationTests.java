40926553340df026f263b360566a1ccdb16bec9d
Juan David <juadsanchezlon@unal.edu.co>
2021-02-20 18:57:11 -0500

    First commit

---
+++ src/test/java/com/calculadoraapp/calculadorahorastrabajo/CalculadoraHorasTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadoraapp.calculadorahorastrabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraHorasTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
